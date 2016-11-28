212504d1a31eed041a1131a57c86015c12cd834d
Pascual Alonso <pascual@yahoo.com>
2016-11-27 19:28:36 -0500

    Initail module3 version

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,55 +4,91 @@
     .controller('NarrowItDownController',NarrowItDownController)
     .service('MenuSearchService',MenuSearchService)
     .constant('DbServer', "https://davids-restaurant.herokuapp.com")
+    .directive('foundItems', FoundItems)
     ;
+
+    function FoundItems() {
+      var ddo = {
+        templateUrl: 'foundItems.html',
+        scope: {
+          items: '<',
+          calls: '<',
+          onRemove: '&'
+        }
+      };
+      return ddo;
+    }
+
+    if (Array.prototype.extend === undefined) {
+      Array.prototype.extend = function(other) {
+        this.push.apply(this, arguments.length > 1 ? arguments : other);
+        return this;
+      };
+    }
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(service) {
       var controller = this;
+      controller.test = "Test";
+      controller.found = []
+      controller.calls = 0;
 
       controller.narrowItDown = function () {
-        controller.found = service.getMatchedMenuItems(controller.searchTerm);
+        service.getMatchedMenuItems(controller.searchTerm,controller.found);
+        controller.calls++;
       }
 
+      controller.remove = function(index) {
+        controller.found.splice(index,1);
+      }
     }
 
     MenuSearchService.$inject = ['$http','DbServer'];
     function MenuSearchService($http,DbServer) {
       var service = this;
-      service.keys = [];
+      service.keys = ["name","description"];
       service.menuItems = [];
 
-      $http({
-        method: "GET",
-        url: (DbServer + "/menu_items.json")
-      }).then(function (result) {
-        if (result && result.data && result.data.menu_items) {
-          service.menuItems = result.data.menu_items;
-          console.log("Received menu items:" + service.menuItems.length);
-          if (service.menuItems.length > 0) {
-            service.keys = Object.keys(service.menuItems[0]);
-            console.log(service.keys);
-          }
+      function filter(searchTerm,found) {
+        found.length = 0;
+        if (searchTerm === undefined || searchTerm === "") {
+          // uncommet this to include all entries.
+          // service.found.extend(service.menuItems);
         } else {
-          console.log("Incorrect data format!");
-        }
-      });
-
-      service.getMatchedMenuItems = function (searchTerm) {
-        var found = [];
-        var items = service.menuItems || [];
-        var keys = service.keys;
-        for (var i=0; i < items.length; i++) {
-          var item = items[i];
-          for (var j=0; j < keys.length; j++) {
-            var value = item[keys[j]] || "";
-            if (value.toString().indexOf(searchTerm) >= 0) {
-              found.push(found);
+          searchTerm = searchTerm.toLowerCase();
+          var keys = service.keys;
+          var items = service.menuItems;
+          for (var i=0; i < items.length; i++) {
+            var item = items[i];
+            for (var j=0; j < keys.length; j++) {
+              var value = item[keys[j]] || "";
+              if (value.toString().toLowerCase().indexOf(searchTerm) >= 0) {
+                found.push(item);
+                break;
+              }
             }
           }
         }
-        console.log(found.length);
-        return found;
+        console.log("Filter:" + found.length);
+      }
+
+      service.getMatchedMenuItems = function (searchTerm,found) {
+        if (service.menuItems.length > 0) {
+          filter(searchTerm,found);
+        } else {
+          $http({
+            method: "GET",
+            url: (DbServer + "/menu_items.json")
+          }).then(function (result) {
+            if (result && result.data && result.data.menu_items) {
+              service.menuItems = result.data.menu_items;
+              console.log("Received menu items:" + service.menuItems.length);
+              filter(searchTerm,found);
+            } else {
+              console.log("Incorrect data format!");
+            }
+          });
+        }
       }
     }
 
